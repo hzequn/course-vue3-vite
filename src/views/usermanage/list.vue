7845fed9204c5010abfebdcdecd2e145e9fffc7e
WIN-0VIAVCKIOQB\EDY <13414851033@163.com>
2023-03-03 15:00:35 +0800

    test(测试一下): 测试一下
    测试一下

--- src/views/usermanage/list.vue
+++ src/views/usermanage/list.vue
@@ -1,5 +1,10 @@
+<!--
+ * @Author: 黄泽群
+ * @Date: 2023-03-03 14:56:16
+ * @Description: file content
+-->
 <template>
-  <div class="content">用户管理</div>
+  <div class="content">用户管理1</div>
 </template>
 
 <script setup>
